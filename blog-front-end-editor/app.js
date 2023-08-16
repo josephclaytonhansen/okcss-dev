b57831cf2624183dc2a9783f00cf06afda6d52b9
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-08-16 18:45:06 -0500

    admin toolbar css skeleton

--- blog-front-end-editor/app.js
+++ blog-front-end-editor/app.js
@@ -12,11 +12,23 @@ app.get('/', (req, res) => {
     res.render('index.html', { root: '.' })})
 
 app.get('/editor.js', (req, res) => {
+    res.type('js')
     res.sendFile("/src/views/js/editor.js", { root: '.' })
 })
 
 app.get('/editor.css', (req, res) => {
+    res.type('css')
     res.sendFile("/src/views/css/editor.css", { root: '.' })
+})
+
+app.get('/admin_toolbar.css', (req, res) => {
+    res.type('css')
+    res.sendFile("/src/includes/css/admin_toolbar.css", { root: '.' })
+})
+
+app.get('/header.css', (req, res) => {
+    res.type('css')
+    res.sendFile("/src/includes/css/header.css", { root: '.' })
 })
 
 app.listen(5920, () => {
