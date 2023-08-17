2f8faff38a31ff850c260ee6e1727f16243a629b
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-08-16 22:03:50 -0500

    functional icon compiler

--- blog-front-end-editor/src/assets/lucide/compiler.py
+++ blog-front-end-editor/src/assets/lucide/compiler.py
@@ -2,7 +2,7 @@ import os, re
 
 icons = []
 
-path = os.path.join(os.getcwd(), "src", "assets", "lucide", "icons")
+path = os.path.join(os.getcwd(), "blog-front-end-editor", "src", "assets", "lucide", "icons")
 for file in os.listdir(path):
     if file.endswith(".svg"):
         icons.append(file)
@@ -10,20 +10,20 @@ for file in os.listdir(path):
 icons = sorted(icons)
 used_icons = []
 
-paths = [os.path.join(os.getcwd(), "src", "views"), os.path.join(os.getcwd(), "src", "includes")]
+paths = [os.path.join(os.getcwd(), "blog-front-end-editor", "src", "views"), os.path.join(os.getcwd(), "blog-front-end-editor", "src", "includes")]
 for path in paths:
-    try:
         for file in os.listdir(path):
-            with open(os.path.join(path, file), "r") as f:
-                data = f.read()
-                used_icons += re.findall(r'lu\.i\("(.+?)"\)', data)
-                used_icons += re.findall(r'lu\.i\("(.+?)"', data)
-    except IsADirectoryError:
-        pass
+            try:
+                with open(os.path.join(path, file), "r") as f:
+                    data = f.read()
+                    used_icons += re.findall(r'lu\.i\("(.+?)"\)', data)
+                    used_icons += re.findall(r'lu\.i\("(.+?)"', data)
+            except IsADirectoryError:
+                pass
 
 used_icons = sorted(list(set(used_icons)))
 
-path = os.path.join(os.getcwd(), "src", "assets", "lucide", "used")
+path = os.path.join(os.getcwd(), "blog-front-end-editor/src", "assets", "lucide", "used")
 for file in os.listdir(path):
     os.remove(os.path.join(path, file))
 
@@ -31,7 +31,7 @@ for i in icons:
     for u in used_icons: 
         u += ".svg"
         if u == i:
-            with open(os.path.join(os.getcwd(), "src", "assets", "lucide", "icons", i), "r") as f:
+            with open(os.path.join(os.getcwd(), "blog-front-end-editor", "src", "assets", "lucide", "icons", i), "r") as f:
                 data = f.read()
                 if not os.path.exists(os.path.join(path, i)):
                     copy = open(os.path.join(path, i), "w")
