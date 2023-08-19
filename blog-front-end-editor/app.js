264089f64514ec402600d7c09bba97e997bec805
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-08-19 18:05:56 -0500

    rate limiter

--- blog-front-end-editor/app.js
+++ blog-front-end-editor/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 const app = express()
 import nunjucks from 'nunjucks'
+import rate_limit from 'express-rate-limit'
 
 import db from './src/server/mongo.js'
 
@@ -12,6 +13,14 @@ nunjucks.configure(['src/views', 'src/includes', 'src/assets'] , {
     watch: true
 })
 
+const limiter = rate_limit({
+    windowMs: 15 * 60 * 1000,
+	max: 100,
+	standardHeaders: true,
+	legacyHeaders: false,
+})
+
+app.use(limiter)
 app.use("", base_routes)
 
 app.listen(5920, () => {
