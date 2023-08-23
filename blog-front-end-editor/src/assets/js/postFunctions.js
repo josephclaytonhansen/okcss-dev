4e1d068c632860253d700f4f1388ec62b7c7a82e
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-08-23 12:26:23 -0500

    centralize post saving

--- blog-front-end-editor/src/assets/js/postFunctions.js
+++ blog-front-end-editor/src/assets/js/postFunctions.js
@@ -1,6 +1,49 @@
+function savePost(){
+    let url = new URL(window.location.href)
+    let post_index = url.href.indexOf('post/')
+    let post_id = url.href.slice(post_index + 5)
+
+    let title = $('#blog-title').val()
+    let slug = $('#blog-slug').val()
+    let metaTitle = $('#blog-meta-title').val()
+    let metaDescription = $('#blog-meta-description').val()
+    let metaKeywords = $('#blog-meta-keywords').val()
+    let status = $('#blog-status').val()
+    let categories = $('#blog-categories').val()
+    let tags = $('#blog-tags').val()
+    let author = $('#blog-author').val()
+    let content = sessionStorage.getItem('current-blog-data-raw')
+    //TODO: featured image
+    let featuredImage = ''
+    //TODO: scheduled date
+    let scheduledDate = ''
+
+    $.ajax({
+        url: '/post/update/' + post_id,
+        type: 'POST',
+        data: {
+            post_id: post_id,
+            title: title,
+            slug: slug,
+            metaTitle: metaTitle,
+            metaDescription: metaDescription,
+            metaKeywords: metaKeywords,
+            status: status,
+            categories: categories,
+            tags: tags,
+            author: author,
+            content: content,
+            featuredImage: featuredImage,
+            scheduledDate: scheduledDate
+        },
+        success: () => savePostCallback(post_id, content),
+        error: function (err) {
+            console.log(err)}
+    })
+
+}
+
 function savePostCallback(post_id, content){
-    console.log(content)
-    alert(content)
     $.ajax({
         method: 'GET',
         url: '/post/update_history/' + post_id,
@@ -11,4 +54,4 @@ function savePostCallback(post_id, content){
     })
 }
 
-export {savePostCallback}
+export {savePostCallback, savePost}
--- blog-front-end-editor/src/includes/js/admin_toolbar.js
+++ blog-front-end-editor/src/includes/js/admin_toolbar.js
@@ -1,4 +1,4 @@
-import {savePostCallback} from '/postFunctions.js'
+import {savePostCallback, savePost} from '/postFunctions.js'
 
 $('#user-dropdown').on('click', (e) => {
     e.stopPropagation()
@@ -48,46 +48,5 @@ function showDropdown(target){
 }
 
 $('#save').on('click', (e) => {
-    let url = new URL(window.location.href)
-    let post_index = url.href.indexOf('post/')
-    let post_id = url.href.slice(post_index + 5)
-
-    let title = $('#blog-title').val()
-    let slug = $('#blog-slug').val()
-    let metaTitle = $('#blog-meta-title').val()
-    let metaDescription = $('#blog-meta-description').val()
-    let metaKeywords = $('#blog-meta-keywords').val()
-    let status = $('#blog-status').val()
-    let categories = $('#blog-categories').val()
-    let tags = $('#blog-tags').val()
-    let author = $('#blog-author').val()
-    let content = sessionStorage.getItem('current-blog-data')
-    //TODO: featured image
-    let featuredImage = ''
-    //TODO: scheduled date
-    let scheduledDate = ''
-
-    $.ajax({
-        url: '/post/update/' + post_id,
-        type: 'POST',
-        data: {
-            post_id: post_id,
-            title: title,
-            slug: slug,
-            metaTitle: metaTitle,
-            metaDescription: metaDescription,
-            metaKeywords: metaKeywords,
-            status: status,
-            categories: categories,
-            tags: tags,
-            author: author,
-            content: content,
-            featuredImage: featuredImage,
-            scheduledDate: scheduledDate
-        },
-        success: () => savePostCallback(post_id, content),
-        error: function (err) {
-            console.log(err)}
-    })
-
+    savePost()
 })
