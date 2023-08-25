8f0bd9d835cbf89b233634dc9314b18312407b6b
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-08-25 11:08:05 -0500

    functional google login

--- seabass/app.js
+++ seabass/app.js
@@ -1,12 +1,17 @@
 import db from './src/server/mongo.js'
 
 import express from 'express'
-const app = express()
+
+import cors from 'cors'
+import fs from 'fs'
+import dotenv from 'dotenv'
+
 import nunjucks from 'nunjucks'
 import rate_limit from 'express-rate-limit'
-const router = express.Router()
+
 import axios from 'axios'
 import upload from './src/server/multerStorage.js'
+import User from './src/server/models/userModel.min.js'
 
 import page_routes from './src/server/routes/pageRoutes.min.js'
 import post_routes from './src/server/routes/postRoutes.min.js'
@@ -17,27 +22,76 @@ import static_routes from './src/server/routes/staticRoutes.min.js'
 
 import session from 'express-session'
 import passport from 'passport'
-import './src/server/passportSetup.js'
+import Strategy from 'passport-google-oauth20'
+import authMiddleware from './src/server/middleware/authMiddleware.js'
+import {default as connectMongoDBSession} from 'connect-mongodb-session'
+
+import cookieParser from 'cookie-parser'
+
+const app = express()
+const router = express.Router()
+
+app.use(cookieParser())
+
+const MongoDBStore = connectMongoDBSession(session)
+
+const store = new MongoDBStore({
+    uri: process.env.MONGO_STRING,
+    databaseName: 'development',
+    collection: 'sessions'
+})
 
 app.use(session({
+    expires: new Date(Date.now() + (1 * 86400 * 1000)),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: true
+        secure: process.env.ENV == 'production',
     },
-    store: db.sessionStore
+    store: store
 }))
 
 app.use(passport.session())
 
 app.use(passport.initialize())
 
-import cors from 'cors'
+passport.use(new Strategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/oauth2/redirect/google',
+    scope: ['profile', 'email'],
+}, async (accessToken, refreshToken, profile, cb) => {
+    const user = await User.findOne({
+        googleId: profile.id
+    })
+    if (user) {
+        return cb(null, user)
+    } else {
+        const newUser = await User.create({
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            image: profile.photos[0].value,
+            email: profile.emails[0].value,
+            permissions: 'worm'
+        })
+        return cb(null, newUser)
+    }
 
-import fs from 'fs'
+}))
 
-import dotenv from 'dotenv'
+passport.serializeUser(async function (user, done) {
+    done(null, user.id)
+})
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id).then((user) => {
+        done(null, user)
+    })
+})
+
 dotenv.config()
 
 // Body parser middleware
@@ -85,14 +139,24 @@ app.use("/comment", comment_routes)
 app.use("/category", category_routes)
 app.use("/", static_routes)
 
-router.get('/', passport.authenticate('2fa-totp', {
+router.get('/login/federated/google', passport.authenticate('google', {
+    scope: ['profile email']
+}))
+
+router.get('/', passport.authenticate('google', {
     successRedirect: '/dashboard',
     failureRedirect: '/login'
 }))
 
-router.get('/edit/post/:id', passport.authenticate('2fa-totp', {
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
+    successRedirect: '/dashboard',
     failureRedirect: '/login',
-}), async (req, res) => {
+}))
+
+router.get('/edit/post/:id', async (req, res) => {
+    if (authMiddleware(req, User) == true) {
+        res.redirect('/login')
+    } else {
     const id = req.params.id
     let all_categories = []
     const categories = await axios.get('http://localhost:5920/category/').then((response) => {
@@ -126,41 +190,44 @@ router.get('/edit/post/:id', passport.authenticate('2fa-totp', {
             all_categories: all_categories
         })
     }
-})
-router.get('/edit/page/:id', passport.authenticate('2fa-totp', {
-    failureRedirect: '/login',
-}), async (req, res) => {
-    const id = req.params.id
-    let all_categories = []
-    const categories = await axios.get('http://localhost:5920/category/').then((response) => {
-        return response.data
-    })
-    for (let i = 0; i < categories.length; i++) {
-        all_categories.push(categories[i].name)
-    }
-    let all_pages = await axios.get('http://localhost:5920/page/').then((response) => {
-        return response.data
-    })
-    let all_page_ids = []
-    for (let i = 0; i < all_pages.length; i++) {
-        all_page_ids.push(all_pages[i]._id)
-    }
-    let id_index = all_page_ids.indexOf(id)
-    if (id_index == -1) {
-        res.redirect('/dashboard')
+}})
+
+router.get('/edit/page/:id', async (req, res) => {
+    if (authMiddleware(req, User) == true) {
+        res.redirect('/login')
     } else {
-        res.render('editor.html', {
-            root: '.',
-            post: await axios.get('http://localhost:5920/page/id/' + all_page_ids[id_index]).then((response) => {
-                return response.data
-            }),
-            type: 'page',
-            user: {
-                email: 'served_from@express.app',
-                username: 'served_from_express',
-            },
-            all_categories: all_categories
+        const id = req.params.id
+        let all_categories = []
+        const categories = await axios.get('http://localhost:5920/category/').then((response) => {
+            return response.data
         })
+        for (let i = 0; i < categories.length; i++) {
+            all_categories.push(categories[i].name)
+        }
+        let all_pages = await axios.get('http://localhost:5920/page/').then((response) => {
+            return response.data
+        })
+        let all_page_ids = []
+        for (let i = 0; i < all_pages.length; i++) {
+            all_page_ids.push(all_pages[i]._id)
+        }
+        let id_index = all_page_ids.indexOf(id)
+        if (id_index == -1) {
+            res.redirect('/dashboard')
+        } else {
+            res.render('editor.html', {
+                root: '.',
+                post: await axios.get('http://localhost:5920/page/id/' + all_page_ids[id_index]).then((response) => {
+                    return response.data
+                }),
+                type: 'page',
+                user: {
+                    email: 'served_from@express.app',
+                    username: 'served_from_express',
+                },
+                all_categories: all_categories
+            })
+        }
     }
 })
 
@@ -171,61 +238,67 @@ router.get('/login', (req, res) => {
 })
 
 router.get('/dashboard', async (req, res) => {
-    //eventually, get this from the database
-    const user = {
-        email: 'served_from@express.app',
-        username: 'served_from_express',
-    }
-    const categories = await axios.get('http://localhost:5920/category/').then((response) => {
-        return response.data
-    })
-    let all_categories = []
-    for (let i = 0; i < categories.length; i++) {
-        all_categories.push(categories[i].name)
-    }
-
-    const authors = await axios.get('http://localhost:5920/user/').then((response) => {
-        return response.data
-    })
-    let all_authors = []
-    for (let i = 0; i < authors.length; i++) {
-        all_authors.push({
-            name: authors[i].display_name,
-            id: authors[i]._id
+    if (authMiddleware(req, User) == true) {
+        res.redirect('/login')
+    } else {
+        //eventually, get this from the database
+        const user = {
+            email: 'served_from@express.app',
+            username: 'served_from_express',
+        }
+        const categories = await axios.get('http://localhost:5920/category/').then((response) => {
+            return response.data
         })
-    }
+        let all_categories = []
+        for (let i = 0; i < categories.length; i++) {
+            all_categories.push(categories[i].name)
+        }
 
-    res.render('dashboard.html', {
-        root: '.',
-        date: {
-            day: new Date().getDate(),
-            month: new Date().getMonth(),
-            monthName: new Date().toLocaleString('default', {
-                month: 'long'
+        const authors = await axios.get('http://localhost:5920/user/').then((response) => {
+            return response.data
+        })
+        let all_authors = []
+        for (let i = 0; i < authors.length; i++) {
+            all_authors.push({
+                name: authors[i].display_name,
+                id: authors[i]._id
+            })
+        }
+
+        res.render('dashboard.html', {
+            root: '.',
+            date: {
+                day: new Date().getDate(),
+                month: new Date().getMonth(),
+                monthName: new Date().toLocaleString('default', {
+                    month: 'long'
+                }),
+                year: new Date().getFullYear()
+            },
+            user: user,
+            all_authors: all_authors,
+            all_categories: categories,
+            current_categories: all_categories,
+            posts: await axios.get('http://localhost:5920/post').then((response) => {
+                return response.data
             }),
-            year: new Date().getFullYear()
-        },
-        user: user,
-        all_authors: all_authors,
-        all_categories: categories,
-        current_categories: all_categories,
-        posts: await axios.get('http://localhost:5920/post').then((response) => {
-            return response.data
-        }),
-        pages: await axios.get('http://localhost:5920/page').then((response) => {
-            return response.data
-        }),
-        upcoming_posts: await axios.get('http://localhost:5920/post/upcoming').then((response) => {
-            return response.data
-        }).catch((error) => {
-            return []
+            pages: await axios.get('http://localhost:5920/page').then((response) => {
+                return response.data
+            }),
+            upcoming_posts: await axios.get('http://localhost:5920/post/upcoming').then((response) => {
+                return response.data
+            }).catch((error) => {
+                return []
+            })
         })
-    })
+    }
 })
 
-router.get('/account', passport.authenticate('2fa-totp', {
-    failureRedirect: '/login',
-}), (req, res) => {
+
+router.get('/account', (req, res) => {
+    if (authMiddleware(req, User) == true) {
+        res.redirect('/login')
+    } else {
     //eventually, get this from the database
     const user = {
         email: 'served_from@express.app',
@@ -250,11 +323,12 @@ router.get('/account', passport.authenticate('2fa-totp', {
         date: date
     })
 
-})
+}})
 
-router.get('/new-post', passport.authenticate('2fa-totp', {
-    failureRedirect: '/login',
-}), async (req, res) => {
+router.get('/new-post', async (req, res) => {
+    if (authMiddleware(req, User) == true) {
+        res.redirect('/login')
+    } else {
     let all_categories = []
     const categories = await axios.get('http://localhost:5920/category/').then((response) => {
         return response.data
@@ -285,11 +359,12 @@ router.get('/new-post', passport.authenticate('2fa-totp', {
         },
         all_categories: all_categories
     })
-})
+}})
 
-router.get('/new-page', passport.authenticate('2fa-totp', {
-    failureRedirect: '/login',
-}), async (req, res) => {
+router.get('/new-page', async (req, res) => {
+    if (authMiddleware(req, User) == true) {
+        res.redirect('/login')
+    } else {
 
     res.render('editor.html', {
         root: '.',
@@ -309,11 +384,12 @@ router.get('/new-page', passport.authenticate('2fa-totp', {
             username: 'served_from_express',
         }
     })
-})
+}})
 
-router.post('/upload-image', passport.authenticate('2fa-totp', {
-    failureRedirect: '/login',
-}), upload.single('streamfile'), (req, res) => {
+router.post('/upload-image', upload.single('streamfile'), (req, res) => {
+    if (authMiddleware(req, User) == true) {
+        res.redirect('/login')
+    } else {
     if (req.fileValidationError) {
         return res.status(400).json({
             message: req.fileValidationError,
@@ -338,7 +414,7 @@ router.post('/upload-image', passport.authenticate('2fa-totp', {
         alt: file.alt_text
     })
 
-});
+}})
 
 app.use('/', router)
 
