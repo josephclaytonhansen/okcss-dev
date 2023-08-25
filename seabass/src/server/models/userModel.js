eea7357e7c126528b51ce34ed50b1852a4812d60
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-08-25 18:08:02 -0500

    fix userModel

--- seabass/src/server/models/userModel.js
+++ seabass/src/server/models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
     googleId: {type:String, unique: true},
-    username: {type:String, unique: true},
+    username: {type:String},
     email: {type:String, unique: true},
     display_name: {type:String, default: "No display name set"},
     picture: String,
