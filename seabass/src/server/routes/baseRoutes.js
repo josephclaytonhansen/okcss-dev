f1c7b07997e09c645f9ac9df827d7d30af1f6145
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-08-29 11:14:49 -0500

    populate account page

--- seabass/src/server/routes/baseRoutes.js
+++ seabass/src/server/routes/baseRoutes.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import User from '../models/userModel.min.js'
+import Post from '../models/postModel.min.js'
 import fs from 'fs'
 import express from 'express'
 const router = express.Router()
@@ -123,35 +124,44 @@ router.get('/dashboard', async (req, res) => {
 })
 
 
-router.get('/account', (req, res) => {
+router.get('/account',async (req, res) => {
     if (!req.session.passport || req.session.passport.permissions == 'worm') {
         res.redirect('/login-na')
     } else {
+        let posts = await axios.get('http://localhost:5920/post/').then((response) => {
+            return response.data
+        })
+        let pages = await axios.get('http://localhost:5920/page/').then((response) => {
+            return response.data
+        })
         let user = req.session.passport.user
-        User.findById(user).then((user) => {
-            //eventually, get this from the database
-            User.findById(req.session.passport.user).then((user) => {
-                const date = {
-                    day: new Date().getDate(),
-                    month: new Date().getMonth(),
-                    monthName: new Date().toLocaleString('default', {
-                        month: 'long'
-                    }),
-                    year: new Date().getFullYear()
-                }
 
-                res.render('account.html', {
-                    root: '.',
-                    user: user,
-                    date: date
-                })
-            }).catch((error) => {
-                res.redirect('/login')
+        //eventually, get this from the database
+        User.findById(req.session.passport.user).then((user) => {
+            const date = {
+                day: new Date().getDate(),
+                month: new Date().getMonth(),
+                monthName: new Date().toLocaleString('default', {
+                    month: 'long'
+                }),
+                year: new Date().getFullYear(),
+            }
+
+            posts = posts.filter((post) => {
+                return post.author == user._id
             })
 
+            pages = pages.filter((page) => {
+                return page.author == user._id
+            })
 
-        }).catch((error) => {
-            res.redirect('/login')
+            res.render('account.html', {
+                root: '.',
+                user: user,
+                date: date,
+                posts: posts,
+                pages: pages
+            })
         })
     }
 })
@@ -237,6 +247,111 @@ router.get('/new-page', async (req, res) => {
                 user: user,
                 all_authors: all_authors
             })
+        }).catch((error) => {
+            res.redirect('/login')
+        })
+    }
+})
+
+router.get('/edit/post/:id', async (req, res) => {
+    if (!req.session.passport || req.session.passport.permissions == 'worm') {
+        res.redirect('/login-na')
+    } else {
+        let user = req.session.passport.user
+
+        const id = req.params.id
+        let all_categories = []
+        const categories = axios.get('http://localhost:5920/category/').then((response) => {
+            return response.data
+        })
+        let all_authors = axios.get('http://localhost:5920/user/').then((response) => {
+            let data = response.data
+            return data.map((author) => {
+                return {
+                    name: author.display_name,
+                    id: author._id
+                }
+            })
+        })
+        for (let i = 0; i < categories.length; i++) {
+            all_categories.push(categories[i].name)
+        }
+        let all_posts = null
+        let all_post_ids = []
+        all_posts = await axios.get('http://localhost:5920/post/')
+        all_posts = all_posts.data
+
+        for (let i = 0; i < all_posts.length; i++) {
+            all_post_ids.push(all_posts[i]._id)
+        }
+        console.log(all_post_ids)
+
+        let id_index = all_post_ids.indexOf(id)
+        if (id_index == -1) {
+            res.redirect('/dashboard')
+
+        } else {
+            res.render('editor.html', {
+                root: '.',
+                post: axios.get('http://localhost:5920/post/id/' + all_post_ids[id_index]).then((response) => {
+                    return response.data
+                }),
+                type: 'post',
+                user: user,
+                username: 'served_from_express',
+                all_categories: all_categories,
+                all_authors: all_authors
+            })
+        }
+    }
+})
+
+
+router.get('/edit/page/:id', async (req, res) => {
+    if (!req.session.passport || req.session.passport.permissions == 'worm') {
+        res.redirect('/login-na')
+    } else {
+        let user = req.session.passport.user
+        User.findById(user).then((user) => {
+            const id = req.params.id
+            let all_categories = []
+            const categories = axios.get('http://localhost:5920/category/').then((response) => {
+                return response.data
+            })
+            let all_authors = axios.get('http://localhost:5920/user/').then((response) => {
+                let data = response.data
+                return data.map((author) => {
+                    return {
+                        name: author.display_name,
+                        id: author._id
+                    }
+                })
+            })
+            for (let i = 0; i < categories.length; i++) {
+                all_categories.push(categories[i].name)
+            }
+            let all_pages = axios.get('http://localhost:5920/page/').then((response) => {
+                return response.data
+            })
+            let all_page_ids = []
+            for (let i = 0; i < all_pages.length; i++) {
+                all_page_ids.push(all_pages[i]._id)
+            }
+            let id_index = all_page_ids.indexOf(id)
+            if (id_index == -1) {
+                res.redirect('/dashboard')
+            } else {
+                res.render('editor.html', {
+                    root: '.',
+                    post: axios.get('http://localhost:5920/page/id/' + all_page_ids[id_index]).then((response) => {
+                        return response.data
+                    }),
+                    type: 'page',
+                    user: user,
+                    all_categories: all_categories,
+                    all_authors: all_authors
+                })
+            }
         }).catch((error) => {
             res.redirect('/login')
         })
