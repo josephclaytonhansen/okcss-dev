330a3304b116e016152eb16546e9513ba8e67900
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-08-31 12:12:21 -0500

    weasel minimal styling

--- weasel/src/main.js
+++ weasel/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import './style.css'
 
 const pinia = createPinia()
 const app = createApp(App)
---
+++ weasel/wards.md
@@ -0,0 +1,7 @@
+* Choctaw
+* Midwest City
+* Oklahoma City 2nd
+* Moore
+* Oklahoma City 6th Branch
+* Mustang 2nd
+* Mustang 1st
