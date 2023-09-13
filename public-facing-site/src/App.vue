a9dd8cd459c5e456e7693b6fad484e79bc0b1584
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-09-13 09:18:41 -0500

    SPA "routing"

--- public-facing-site/src/App.vue
+++ public-facing-site/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 
-    import HelloWorld from './components/HelloWorld.vue'
+    import Toast from './components/Toast.vue'
     import Navbar from './components/Navbar.vue'
     import Footer from './components/Footer.vue'
+    import Main from './components/Main.vue'
 
-    import { ref } from 'vue'
+    import { ref, reactive } from 'vue'
 
     const ig_link = ref("https://www.instagram.com/okcsouthstake/")
     const fb_link = ref("https://www.facebook.com/okcsouthstake")
@@ -14,16 +15,69 @@
 
     const wards = ref(["Choctaw", "Midwest City", "OKC 2nd", "Moore", "OKC 6th Branch", "Mustang 2nd", "Mustang 1st"])
 
+    const toast = reactive({
+        duration: 2000,
+        class: "text-bg-info",
+        message: "",
+    })
+
+    const currentURL = ref(window.location.pathname)
+
+    const contentURLMappings = {
+        "/": "home",
+        "/home": "home",
+        "/blog": "news",
+        "/news": "news",
+        "/about": "contact-us",
+        "/contact": "contact-us",
+        "contact-us": "contact-us",
+        "/calendar": "events",
+        "/events": "events",
+        "/classes": "classes",
+        "/temple-prep": "classes",
+        "/self-reliance": "classes",
+        "/beliefs": "beliefs",
+        "/our-beliefs": "beliefs",
+        "/what-we-believe": "beliefs",
+        "missionaries": "missionaries",
+        "/service": "service",
+        "/service-oppurtunities": "service",
+        "/temple": "okc-temple",
+        "/temple-info": "okc-temple",
+        "/okc-temple": "okc-temple",
+        "/what-are-temples": "what-are-temples",
+        "/what-is-a-temple": "what-are-temples",
+        "/what-is-the-temple": "what-are-temples",
+    }
+
+const getCurrentPage = () => {
+    currentURL.value = window.location.pathname
+    return contentURLMappings[currentURL.value]
+}
+
+
 </script>
 
 <template>
     <div id = "app">
         <Navbar :wards="wards"/>
-        <div  class = "container">
-            <HelloWorld/>
-        </div>
-    <Footer :wards="wards" :ig_link="ig_link" :fb_link="fb_link" :phone="phone" :email="email" :blurb="blurb"/>
+        <Main>
+            <Home v-if = "getCurrentPage() === 'home'"/>
+            <News v-else-if = "getCurrentPage() === 'news'"/>
+            <ContactUs v-else-if = "getCurrentPage() === 'contact-us'"/>
+            <Events v-else-if = "getCurrentPage() === 'events'"/>
+            <Classes v-else-if = "getCurrentPage() === 'classes'"/>
+            <Beliefs v-else-if = "getCurrentPage() === 'beliefs'"/>
+            <Missionaries v-else-if = "getCurrentPage() === 'missionaries'"/>
+            <Service v-else-if = "getCurrentPage() === 'service'"/>
+            <OKCTemple v-else-if = "getCurrentPage() === 'okc-temple'"/>
+            <WhatAreTemples v-else-if = "getCurrentPage() === 'what-are-temples'"/>
+            <Home v-else/>
+        </Main>
+        <Toast :toast="toast"/>
+        <Footer :wards="wards" :ig_link="ig_link" :fb_link="fb_link" :phone="phone" :email="email" :blurb="blurb"/>
     </div>
+
 </template>
 
 <style scoped>
---
+++ public-facing-site/src/components/Toast.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+    const props = defineProps({
+        toast: {
+            duration: 2000,
+            class: "text-bg-info",
+            message: "Hello World!",
+        },
+    })
+
+</script>
+
+<template>
+    <div class="toast-container position-fixed top-0 end-0 p-3">
+        <div class="toast align-items-center border-0" :class="toast.class" role="alert" aria-live="assertive" aria-atomic="true" :data-bs-delay="toast.duration">
+            <div class="d-flex">
+                <div class="toast-body">
+                    {{ toast.message }}
+                </div>
+                <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+</template>
