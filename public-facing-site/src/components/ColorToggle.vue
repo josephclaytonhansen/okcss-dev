15cd930cd0d31e7499359298d92b8dee29b5fd77
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-09-14 13:12:18 -0500

    Update ColorToggle.vue

--- public-facing-site/src/components/ColorToggle.vue
+++ public-facing-site/src/components/ColorToggle.vue
@@ -17,7 +17,6 @@ onMounted(() => {
     <li class="nav-item dropdown" id = "picker">
             <button class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (light)">
                 <div class = "button-sun"><Sun/></div><div class = "button-moon"><Moon/></div>
-              <span class="d-lg-none ms-2" id="bd-theme-text">Toggle theme</span>
             </button>
             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="bd-theme-text">
               <li>
