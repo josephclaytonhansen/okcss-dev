283ce442090c92ee4c5bd1aaf768fb8c3e8d64ae
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-09-22 08:15:29 -0600

    remove data.js from wb

--- weasel-backend/seeder.js
+++ weasel-backend/seeder.js
@@ -3,9 +3,10 @@ import Event from './models/event.js'
 import Person from './models/person.js'
 import Tool from './models/tool.js'
 import Worship from './models/worship.js'
+import User from './models/user.js'
 import db from './mongo.js'
 
-import {tools, events, contacts} from './data.js'
+import {tools, events, contacts, users} from './data.js'
 
 const importData = async () => {
     try {
@@ -16,6 +17,19 @@ const importData = async () => {
         const createdEvents = await Event.insertMany(events)
         const createdContacts = await Person.insertMany(contacts)
         console.log(createdTools, createdEvents, createdContacts)
+
+        users.forEach(user => {
+            if (!User.exists()){
+                const newUser = new User({
+                    email: user.email,
+                    password: user.password,
+                    ward: user.ward,
+                    organization: user.organization
+                })
+                newUser.save()
+            }
+        })
+        
         process.exit()
         
     } catch (error) {
