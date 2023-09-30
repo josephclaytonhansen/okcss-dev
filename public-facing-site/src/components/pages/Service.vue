942125281be177a25b5d11594e6b38a3da5f3d78
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-09-29 19:55:15 -0500

    service second image

--- public-facing-site/src/components/pages/Service.vue
+++ public-facing-site/src/components/pages/Service.vue
@@ -4,7 +4,7 @@
 <template>
 <div class = "row align-items-center py-5 gy-3" id = 'hero-row'>
         <div class = "col-12 col-md-6 col-sm-12">
-            <div class = "tall-img-container">
+            <div class = "square-img-container">
                 <img class = "square-img" src = "/assets/Pictures/Service/christ.png"/>
             </div>
 
