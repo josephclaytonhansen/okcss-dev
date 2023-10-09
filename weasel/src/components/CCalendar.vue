cb4280333817003913dce88ff674a419f722e2d2
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-10-08 20:39:20 -0500

    restore calendar tabs

--- weasel/src/components/CCalendar.vue
+++ weasel/src/components/CCalendar.vue
@@ -15,6 +15,31 @@
 </script>
 
 <template>
+    <div class = "tab-row row flex-between wrap">
+        <div class = "tab-row-tabs row tab-row-tabs-calendar wrap">
+            <div class = "tab calendar-tab" :class = "{activeTab: currentOrganization === 'ward'}" @click = "currentOrganization = 'ward'">
+                <div class="tab-overlay calendar-tab-overlay" :class="{active: currentOrganization === 'ward'}"/>
+                <h3>Ward</h3>
+            </div>
+            <div class = "tab calendar-tab" :class = "{activeTab: currentOrganization === 'primary'}" @click = "currentOrganization = 'primary'">
+                <div class="tab-overlay calendar-tab-overlay" :class="{active: currentOrganization === 'primary'}"/>
+                <h3>Primary</h3>
+            </div>
+            <div class = "tab calendar-tab" :class = "{activeTab: currentOrganization === 'ywym'}" @click = "currentOrganization = 'ywym'">
+                <div class="tab-overlay calendar-tab-overlay" :class="{active: currentOrganization === 'ywym'}"/>
+                <h3>YW/YM</h3>
+            </div>
+            <div class = "tab calendar-tab" :class = "{activeTab: currentOrganization === 'relief society'}" @click = "currentOrganization = 'relief society'">
+                <div class="tab-overlay calendar-tab-overlay" :class="{active: currentOrganization === 'relief society'}"/>
+                <h3>Relief Society</h3>
+            </div>
+            <div class = "tab calendar-tab" :class = "{activeTab: currentOrganization === 'elder\'s quorum'}" @click = "currentOrganization = 'elder\'s quorum'">
+                <div class="tab-overlay calendar-tab-overlay" :class="{active: currentOrganization === 'elder\'s quorum'}"/>
+                <h3>Elder's Quorum</h3>
+            </div>
+        </div>
+
+    </div>
     <div>
         <div class = "row stretch wrap" id = "calendar-container">
             <div class = "col-4 fwc">
