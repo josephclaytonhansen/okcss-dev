37b82467208e60e0c7989ee35b0cc25974854383
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-10-09 20:04:27 -0500

    highcouncil router build

--- highcouncil/src/main.js
+++ highcouncil/src/main.js
@@ -1,6 +1,18 @@
-import {createApp} from "vue";
-import * as bootstrap from 'bootstrap';
-window.bootstrap = bootstrap;
-import "./style.css";
-import App from "./App.vue";
-createApp(App).mount("#app");
+import {createApp} from "vue"
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap
+import "./style.css"
+import App from "./App.vue"
+import 'vue-router'
+const routes = [
+  { path: '/', component: Home },
+]
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHashHistory(),
+  routes,
+})
+
+app.use(router)
+
+createApp(App).mount("#app")
