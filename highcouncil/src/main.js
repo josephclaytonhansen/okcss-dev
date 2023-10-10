e6b00cb5b3e10d518c7cbd82bb368fc6ebb8fbc1
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-10-10 18:33:26 -0500

    highcouncil pinia

--- highcouncil/src/main.js
+++ highcouncil/src/main.js
@@ -1,16 +1,38 @@
+import {
+    createApp
+} from 'vue'
+import {
+    createPinia
+} from 'pinia'
+
+import App from './App.vue'
+import './style.css'
+import axios from 'axios'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+
+export {
+    pinia
+}
+
+
 import {createApp} from "vue"
 import * as bootstrap from 'bootstrap'
 window.bootstrap = bootstrap
 import "./style.css"
 import App from "./App.vue"
-import 'vue-router'
+import {
+    createRouter,
+    createWebHistory
+} from 'vue-router'
 const routes = [
   { path: '/', component: Home },
 ]
 
-const router = VueRouter.createRouter({
-  history: VueRouter.createWebHashHistory(),
-  routes,
+const router = createRouter({
+    history: createWebHistory(),
+    routes: []
 })
 
 app.use(router)
