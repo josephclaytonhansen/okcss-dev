6cfa5ffb89dc1d3740e5ec84894b3d39d5540e2e
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-10-10 17:50:08 -0500

    Update hc_reportRoutes.js

--- weasel-backend/routes/hc_reportRoutes.js
+++ weasel-backend/routes/hc_reportRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express'
 const router = express.Router()
+import dotenv from 'dotenv'
+dotenv.config()
 
 import {
     getHcReports,
@@ -12,5 +14,9 @@ router.route('/').get(getHcReports).post(createHcReport)
 router.route('/:id').get(getHcReportById).put(updateHcReport).delete(deleteHcReport)
 router.route('/counselor/:counselor').get(getHcReportsByCounselor)
 router.route('/date/:date').get(getHcReportsByDate)
+const hc_pin = process.env.HC_PIN
+router.route('/pin/get').get((req, res) => {
+    res.json(hc_pin)
+})
 
 export default router
