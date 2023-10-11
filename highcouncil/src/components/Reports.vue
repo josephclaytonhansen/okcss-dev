94065749316d0e8e2107ec42da7988b09d1e544e
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-10-11 11:35:25 -0500

    weekly indicators build

--- highcouncil/src/components/Reports.vue
+++ highcouncil/src/components/Reports.vue
@@ -5,9 +5,22 @@ const props = defineProps(['reports'])
 const weeks = ref([])
 const weeksPartOne = ref([])
 const weeksPartTwo = ref([])
-const weeksPartThree = ref([])
+const weeksPartThree = ref([]) 
+
+const weeksWithReports = ref([
+    'Oct 1 - Oct 7',
+    'Mar 19 - Mar 25',
+    'Jul 2 - Jul 8'
+])
 
 const currentWeek = ref('')
+const isCurrentWeek = (week) => {
+    return week === currentWeek.value
+}
+
+const isWeekWithReports = (week) => {
+    return weeksWithReports.value.includes(week)
+}
 
 const setUpWeeks = () => {
     //Format is Jan 1 - Jan 7
@@ -82,6 +95,10 @@ const incrementCurrentWeek = () => {
     }
 }
 
+const setCurrentWeekFromDropdown = (week) => {
+    currentWeek.value = week
+}
+
 </script>
 
 <template>
@@ -92,7 +109,7 @@ const incrementCurrentWeek = () => {
             </div>
             <div class = "col-12 col-sm-12 col-md-auto">
                 <div class = "row align-items-center" style = "margin-top:-.6rem;">
-                    <div class = "col-6 px-5">
+                    <div class = "col-6 px-4">
                         <div class="dropdown-counselor">
                             <button class="btn btn-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Counselor
@@ -107,29 +124,41 @@ const incrementCurrentWeek = () => {
                         </div>
                     </div>
                     <div class = "col-6">
-                        <div class = "row align-items-center" style = "flex-wrap:nowrap">
-                            <div class = "col px-0">
+                        <div class = "row align-items-center" style = "flex-wrap:nowrap" id = "week-buttons">
+                            <div class = "col px-0 week-button">
                                 <a class = "btn btn-info" @click = "decrementCurrentWeek">&#9664;</a>
                             </div>
-                            <div class ="dropdown-weeks col px-1">
+                            <div class ="dropdown-weeks col px-1 week-button">
                             <button class="btn btn-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 {{currentWeek}}
                             </button>
                             <ul class="dropdown-menu" id = "weeks-items">
                                 <div class = "row">
-                                    <div class = "col-4">
-                                        <li v-for = "week in weeksPartOne" :key = "week"><a class="dropdown-item" href="#">{{week}}</a></li>
+                                    <div class = "col-12 col-sm-12 col-md-4">
+                                        <li v-for = "week in weeksPartOne" :key = "week"><a class="dropdown-item" @click = "setCurrentWeekFromDropdown(week)">
+                                        <div v-if = "isCurrentWeek(week)" class = "current-week-indicator"></div>
+                                        <div v-if = "isWeekWithReports(week)" class = "week-with-reports-indicator"></div>
+                                        {{week}}
+                                        </a></li>
                                     </div>
-                                    <div class = "col-4">
-                                        <li v-for = "week in weeksPartTwo" :key = "week"><a class="dropdown-item" href="#">{{week}}</a></li>
+                                    <div class = "col-12 col-sm-12 col-md-4">
+                                        <li v-for = "week in weeksPartTwo" :key = "week"><a class="dropdown-item" @click = "setCurrentWeekFromDropdown(week)">
+                                        <div v-if = "isCurrentWeek(week)" class = "current-week-indicator"></div>
+                                        <div v-if = "isWeekWithReports(week)" class = "week-with-reports-indicator"></div>
+                                        {{week}}
+                                        </a></li>
                                     </div>
-                                    <div class = "col-4">
-                                        <li v-for = "week in weeksPartThree" :key = "week"><a class="dropdown-item" href="#">{{week}}</a></li>
+                                    <div class = "col-12 col-sm-12 col-md-4">
+                                        <li v-for = "week in weeksPartThree" :key = "week"><a class="dropdown-item" @click = "setCurrentWeekFromDropdown(week)">
+                                        <div v-if = "isCurrentWeek(week)" class = "current-week-indicator"></div>
+                                        <div v-if = "isWeekWithReports(week)" class = "week-with-reports-indicator"></div>
+                                        {{week}}
+                                        </a></li>
                                     </div>
                                 </div>
                             </ul>
                         </div>
-                            <div class = "col px-0">
+                            <div class = "col px-0 week-button">
                                 <a class = "btn btn-info" @click = "incrementCurrentWeek">&#9654;</a>
                             </div>
                         </div>
@@ -140,3 +169,34 @@ const incrementCurrentWeek = () => {
         <hr style = "width:102%"/>
     </div>
 </template>
+
+<style scoped>
+.current-week-indicator{
+    display:inline-block;
+    width:.5rem;
+    height:.5rem;
+    position:absolute;
+    margin-left: -.8rem;
+    border:solid 2px var(--bs-info);
+    border-radius:50%;
+    margin-top: .55rem;
+    float:left;
+}
+.week-with-reports-indicator{
+    display:inline-block;
+    position:absolute;
+    width:.5rem;
+    margin-left: -.8rem;
+    height:.5rem;
+    background-color:var(--bs-success);
+    border-radius:50%;
+    margin-top: .55rem;
+    float:left;
+}
+
+@media screen and (max-width: 1000px) {
+    #week-buttons {
+        margin-left:-4rem;
+    }
+}
+</style>
