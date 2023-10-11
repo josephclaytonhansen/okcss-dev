8152eea79e366597fb72bac027742478f8373f96
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-10-10 19:23:35 -0500

    hc pin length build

--- highcouncil/src/components/Main.vue
+++ highcouncil/src/components/Main.vue
@@ -13,11 +13,14 @@ const getPinFromApi = async () => {
 }
 
 const comparePin = async () => {
+    enteredPinLength.value = enteredPin.value.length
     const pin = await getPinFromApi()
     if (enteredPin.value === pin) {
         currentComponent.value = 'reports'
     }
 }
+
+const enteredPinLength = ref(0)
 
 </script>
 
@@ -25,11 +28,12 @@ const comparePin = async () => {
     <div>
         <div v-if = "currentComponent === 'pin'">
             <div class = "container d-flex justify-content-center align-items-center" style="height:100vh;">
-                <div class = "card p-5" >
+                <div class = "card p-4" >
                     <div class = "card-body text-center">
                         <h1>High Council Reports</h1>
                         <p class = "fs-4 text-muted">Enter PIN</p>
                         <input type="password" v-model="enteredPin" @keyup="comparePin" class="input-group p-3 fs-2 text-center mx-auto" style = "width:9rem;">
+                        <p class = "text-muted text-small small mt-2">{{enteredPinLength.valueOf()}}/6</p>
                     </div>
             </div>
         </div>
