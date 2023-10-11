9898293337ccd1e8fe88dd38e3990e2fe710c52e
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-10-10 19:00:35 -0500

    no router build

--- highcouncil/src/main.js
+++ highcouncil/src/main.js
@@ -1,23 +1,8 @@
-import axios from 'axios'
-
 import {createApp} from "vue"
 import * as bootstrap from 'bootstrap'
 window.bootstrap = bootstrap
 import "./style.css"
 import App from "./App.vue"
-import {
-    createRouter,
-    createWebHistory
-} from 'vue-router'
-const routes = [
-  { path: '/', component: Home },
-]
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes: []
-})
-
-app.use(router)
 
 createApp(App).mount("#app")
