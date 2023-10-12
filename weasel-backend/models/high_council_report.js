f6ebc865bec884ecb0f4d1c98716aad39afff11c
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-10-12 12:57:30 -0500

    sanitize hc reports

--- weasel-backend/models/high_council_report.js
+++ weasel-backend/models/high_council_report.js
@@ -7,5 +7,37 @@ const highCouncilReportSchema = new mongoose.Schema({
     content_link: String
 })
 
+highCouncilReportSchema.pre('save', function (next) {
+    this.content_text = this.content_text.replace(/<script>/gi, '')
+    this.content_text = this.content_text.replace(/<\/script>/gi, '')
+    this.content_text = this.content_text.replace(/<\*script\*>/gi, '')
+    this.content_text = this.content_text.replace(/<\/\*script\*>/gi, '')
+    this.content_text = this.content_text.replace(/<\?php>/gi, '')
+    this.content_text = this.content_text.replace(/<\?php/gi, '')
+    this.content_text = this.content_text.replace(/<iframe>/gi, '')
+    this.content_text = this.content_text.replace(/<\/iframe>/gi, '')
+    this.content_text = this.content_text.replace(/<frame>/gi, '')
+    this.content_text = this.content_text.replace(/<\/frame>/gi, '')
+    this.content_text = this.content_text.replace(/<frameset>/gi, '')
+    this.content_text = this.content_text.replace(/<\/frameset>/gi, '')
+    this.content_text = this.content_text.replace(/<object>/gi, '')
+    this.content_text = this.content_text.replace(/<\/object>/gi, '')
+    this.content_text = this.content_text.replace(/<embed>/gi, '')
+    this.content_text = this.content_text.replace(/<\/embed>/gi, '')
+    this.content_text = this.content_text.replace(/<applet>/gi, '')
+    this.content_text = this.content_text.replace(/<\/applet>/gi, '')
+
+    this.content_text = this.content_text.replace(/style="[^"]*"/gi, '')
+    this.content_text = this.content_text.replace(/on\w+="[^"]*"/gi, '')
+
+    let headStart = this.content_text.indexOf('<head>')
+    let headEnd = this.content_text.indexOf('</head>')
+    if (headStart !== -1 && headEnd !== -1) {
+        let head = this.content_text.substring(headStart, headEnd + 7)
+        this.content_text = this.content_text.replace(head, '')
+    }
+    next()
+})
+
 const HighCouncilReport = mongoose.model('HighCouncilReport', highCouncilReportSchema)
 export default HighCouncilReport
