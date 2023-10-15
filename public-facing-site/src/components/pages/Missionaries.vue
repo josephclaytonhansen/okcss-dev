100336656bbccfc1c31d37b43237e5928f61745a
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-10-15 17:56:04 -0500

    internal missionaries

--- public-facing-site/src/components/pages/Missionaries.vue
+++ public-facing-site/src/components/pages/Missionaries.vue
@@ -1,4 +1,14 @@
 <script setup>
+const internal_missionaries = ref([])
+const external_missionaries = ref([])
+import axios from 'axios'
+import { onBeforeMount } from 'vue'
+onBeforeMount(async () => {
+    const internal_missionaries_response = await axios.get('https://weasel.okcsouthstake.org/api/missionaries/internal')
+    internal_missionaries.value = internal_missionaries_response.data
+    const external_missionaries_response = await axios.get('https://weasel.okcsouthstake.org/api/missionaries/external')
+    external_missionaries.value = external_missionaries_response.data
+})
 </script>
 
 <template>
@@ -13,7 +23,6 @@
                 <h1 id = "title-small"><a href = 'https://www.churchofjesuschrist.org/comeuntochrist/requests/missionary-visit'>Meet the Missionaries</a></h1>
                 <p class = "py-2 hero-text">
                     Just as Christ and his apostles spread the gospel in their time, we spread the gospel now. The Church of Jesus Christ of Latter-Day Saints has over 72,000 volunteer missionaries serving around the world. Each missionary sets aside their personal life, work, and school for up to two years to preach the gospel. 
-                    <br><br>We have missionaries from around the country and the world serving in our area.
 
                 </p>
             </div>
@@ -23,7 +32,15 @@
     <hr class = "w-50 mx-auto"/>
 
     <div class = "row align-items-center py-5 gy-3">
-        <div class = "col-3">
+        <h2 id = "title" class = "text-center">Missionaries in the Stake</h2>
+        <p class = "text-center">We have missionaries from around the country and the world serving in our area.</p>
+        <div class = "col-6 col-sm-6 col-md-4">
+            <div class = "row gy-3" id = "bios">
+                <div v-for = "missionary in internal_missionaries" class = "col-12 col-sm-6 col-md-12">
+                    <h4 id = "title-small" class = "text-center">{{missionary.ward}}</h4>
+                    <p class = "text-center">{{missionary.phone}}</p>
+                </div>
+            </div>
         </div>
     </div>
 
