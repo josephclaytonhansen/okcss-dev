dcbfcf20069de124a64fad03a085ca00f11c1611
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-10-23 09:54:42 -0500

    Update seeder.js

--- weasel-backend/seeder.js
+++ weasel-backend/seeder.js
@@ -1,24 +1,14 @@
 //create sample data using Event model, Person model, Tool model, and Worship model
-import Event from './models/event.js'
-import Person from './models/person.js'
-import Tool from './models/tool.js'
-import Worship from './models/worship.js'
 import User from './models/user.js'
 import db from './mongo.js'
 
-import {tools, events, contacts, users} from './data.js'
+let users = [
+]
 
 const importData = async () => {
     try {
-        await Tool.deleteMany()
-        await Event.deleteMany()
-        await Person.deleteMany()
-        const createdTools = await Tool.insertMany(tools)
-        const createdEvents = await Event.insertMany(events)
-        const createdContacts = await Person.insertMany(contacts)
-        console.log(createdTools, createdEvents, createdContacts)
-
-        
+        const createdUsers = await User.insertMany(users)
+        console.log(createdUsers)
         
         process.exit()
         
