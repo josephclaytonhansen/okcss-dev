c4a8b4ba3dd8542bd2a7ad86ecd3f16fc6b18f6f
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-12-12 16:21:11 -0600

    training cleanup

--- training/src/components/Main.vue
+++ training/src/components/Main.vue
@@ -17,11 +17,11 @@
 
     const slugify = (text) => {
         return text.toString().toLowerCase()
-            .replace(/\s+/g, '-')           // Replace spaces with -
-            .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-            .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-            .replace(/^-+/, '')             // Trim - from start of text
-            .replace(/-+$/, '')             // Trim - from end of text
+            .replace(/\s+/g, '-')
+            .replace(/[^\w\-]+/g, '')
+            .replace(/\-\-+/g, '-')
+            .replace(/^-+/, '')
+            .replace(/-+$/, '')
     }
 
     onMounted(async () => {
@@ -49,7 +49,3 @@
         <Footer/>
     </div>
 </template>
-
-<style scoped>
-
-</style>
