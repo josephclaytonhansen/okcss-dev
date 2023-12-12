d4c6ea85e77e709678d48b89199418f79cec3768
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-12-12 14:27:52 -0600

    direct output test

--- training/src/components/Article.vue
+++ training/src/components/Article.vue
@@ -2,16 +2,13 @@
     import { ref, computed } from 'vue'
     const props = defineProps(['currentArticle'])
     import {marked} from 'marked'
-    const markedArticle = computed((c) => {
-        return marked(c)
-    })
 
 </script>
 
 <template>
     <div id = "article">
         <h1>{{currentArticle.title}}</h1>
-        <div>{{markedArticle(currentArticle.md_content)}}</div>
+        <div>{{currentArticle.md_content}}</div>
     </div>
 </template>
 
