bc6100f0b4dc990a5094afccae0d8bca65b84b30
josephhansen-bcss <141339577+josephhansen-bcss@users.noreply.github.com>
2023-12-12 09:45:59 -0600

    Update trainingRoutes.js

--- weasel-backend/routes/trainingRoutes.js
+++ weasel-backend/routes/trainingRoutes.js
@@ -4,12 +4,9 @@ const router = express.Router()
 import {
     getTrainings,
     getTrainingById,
-    createTraining,
-    deleteTraining
 } from '../controllers/trainingController.js'
 
 router.route('/').get(getTrainings)
-router.route('/:id').get(getTrainingById).delete(deleteTraining)
-router.route('/create/new').post(createTraining)
+router.route('/:id').get(getTrainingById)
 
 export default router
