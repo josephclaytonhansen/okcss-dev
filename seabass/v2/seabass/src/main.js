bea6e00562c10fd0bd17559af308cd350665ae80
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-12-16 16:47:58 -0600

    initial build

--- seabass/v2/seabass/src/main.js
+++ seabass/v2/seabass/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
+import './style.css'
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
 
 const app = createApp()
 app.component('QuillEditor', QuillEditor)
 app.mount('#app')
+
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap
