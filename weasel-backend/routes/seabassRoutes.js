900b846412bb3c9d96a22e6c5704cfd80746f9dd
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-12-23 16:51:49 -0600

    Update seabassRoutes.js

--- weasel-backend/routes/seabassRoutes.js
+++ weasel-backend/routes/seabassRoutes.js
@@ -9,12 +9,14 @@ import {
     deleteSeabassData,
     updateSeabassData
 } from '../controllers/seabassDataController.js'
+import { get } from 'mongoose'
 const seabassUsername = process.env.SEABASS_USERNAME
 const seabassPassword = process.env.SEABASS_PASSWORD
 router.route('/').post((req, res) => {
     try {
         if (req.body.username === seabassUsername && req.body.password === seabassPassword) {
-            res.status(200).send({ message: 'Valid login' })
+            let data = getSeabassData()
+            res.status(200).send({ message: 'Valid login', data: data })
         } else {
             res.status(401).send({ message: 'Invalid login'  })
         }} catch (err) {
