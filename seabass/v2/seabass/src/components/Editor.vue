33f7e9bd1ab4f60fb75b580d848e5e9f6a14dc9d
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-12-26 13:10:55 -0600

    seabass editor

--- seabass/v2/seabass/src/components/Editor.vue
+++ seabass/v2/seabass/src/components/Editor.vue
@@ -86,7 +86,7 @@ export default {
       }
     ]
 
-    return { content, title, modules, updateContent, saveBlog, toast, updateCurrentComponent, categories }
+    return { content, title, modules, updateContent, saveBlog, toast, updateCurrentComponent, categories, category, selectCategory }
   },
 }
 </script>
