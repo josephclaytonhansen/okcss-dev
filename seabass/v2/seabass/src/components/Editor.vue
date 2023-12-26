f8676e53c1eb78a988d669d25c95e71838297d30
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-12-26 12:03:08 -0600

    simple test

--- seabass/v2/seabass/src/components/Editor.vue
+++ seabass/v2/seabass/src/components/Editor.vue
@@ -1,111 +1,36 @@
-<script>
-import { ref, onMounted, watch } from 'vue'
-import axios from 'axios'
-import { QuillEditor, Delta } from '@vueup/vue-quill'
-import ImageCompress from 'quill-image-compress'
-import MagicUrl from 'quill-magic-url'
-import '@vueup/vue-quill/dist/vue-quill.snow.css'
-import { useToast } from "vue-toastification"
+<template>
+  <div>
+    <QuillEditor v-model:content="internalContent" @update:modelValue="updateContent" />
+  </div>
+</template>
 
-const toast = useToast()
+<script>
+import { ref, watch } from 'vue'
+import { QuillEditor, Delta } from '@vueup/vue-quill'
 
 export default {
-  props: {
-    username: String,
-    password: String,
-    blogId: String,
-    blogs: Array,
-    currentComponent: String
-  },
-  emits: ['updateCurrentComponent'],
   components: {
     QuillEditor
   },
-  setup: (props, { emit }) => {
-    const blog = ref({
-      content: Delta()
-    })
-
-    const saveBlog = async () => {
-      try {
-        let id = props.blogId
-        let date = new Date().toISOString()
-        let response = await axios.put('https://weasel.okcsouthstake.org/api/seabass/' + id, {
-          username: props.username,
-          password: props.password,
-          content: blog.value.content,
-          title: blog.value.title,
-          status: blog.value.status,
-          category: blog.value.category,
-          date: date
-        })
-
-        if (response.status === 200) {
-          toast.success("Blog saved")
-        } else {
-          toast.error("Error saving blog: " + response.status)
-        }
-      } catch (error) {
-        toast.error("Error saving blog: " + error)
-      }
+  props: {
+    content: {
+      type: Object,
+      default: () => new Delta()
     }
+  },
+  setup(props, { emit }) {
+    const internalContent = ref(props.content)
 
-    onMounted(async () => {
-      const foundBlog = props.blogs.find((blog) => blog._id === props.blogId)
-      blog.value = foundBlog
-      if (blog.value.content === null || blog.value.content == "" || blog.value.content == undefined ) {
-        blog.value.content = Delta()
-      }
+    watch(internalContent, (newContent) => {
+      console.log('Content changed:', newContent)
+      emit('update:modelValue', newContent)
     })
-
-    watch(blog, () => {
-      if (blog){
-      console.log(blog.value.content)}
-    });
-
-    const modules = [
-      {
-        name: 'quillImageCompress',
-        module: ImageCompress,
-      },
-      {
-        name: 'magicUrl',
-        module: MagicUrl,
-      }
-    ]
-
-    const updateCurrentComponent = () => {
-      emit('updateCurrentComponent', 'dashboard')
-    }
 
     const updateContent = (newContent) => {
-      blog.value.content = { ops: newContent };
+      internalContent.value = newContent
     }
 
-    return { blog, modules, updateCurrentComponent, saveBlog, toast, updateContent }
-  },
+    return { internalContent, updateContent }
+  }
 }
 </script>
-
-<template>
-  <div class="container-fluid">
-    <div class="row align-items-center">
-      <div class="col-auto">
-        <button class="btn btn-secondary" @click="updateCurrentComponent">Back</button>
-      </div>
-      <div class="col-auto">
-        <input type="text" v-model="blog.title" class="fs-2" />
-      </div>
-      <div class="col-auto">
-        <button class="btn btn-primary" @click="saveBlog">Save</button>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="col-12">
-        <QuillEditor theme="snow" toolbar="full" :modules="modules" v-model:content="blog.content"
-    content-type="delta" />
-      </div>
-    </div>
-  </div>
-</template>
