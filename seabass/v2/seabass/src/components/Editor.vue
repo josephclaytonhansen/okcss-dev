b3bf66da9c36ab5e15c3886fba8b195e75fb5a49
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-12-26 11:37:31 -0600

    ck test

--- seabass/v2/seabass/src/components/Editor.vue
+++ seabass/v2/seabass/src/components/Editor.vue
@@ -1,10 +1,8 @@
 <script>
-import { ref, onMounted, watch } from 'vue'
+import { ref, onMounted } from 'vue'
 import axios from 'axios'
-import { QuillEditor } from '@vueup/vue-quill'
-import ImageCompress from 'quill-image-compress'
-import MagicUrl from 'quill-magic-url'
-import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import { CKEditor } from '@ckeditor/ckeditor5-vue'
 import { useToast } from "vue-toastification"
 
 const toast = useToast()
@@ -19,11 +17,11 @@ export default {
   },
   emits: ['updateCurrentComponent'],
   components: {
-    QuillEditor
+    ckeditor: CKEditor.component
   },
   setup: (props, { emit }) => {
     const blog = ref({
-      content: { ops: [] }
+      content: ''
     })
 
     const saveBlog = async () => {
@@ -52,41 +50,19 @@ export default {
 
     onMounted(async () => {
       const foundBlog = props.blogs.find((blog) => blog._id === props.blogId)
-      if (foundBlog && typeof foundBlog.content === 'object' && foundBlog.content.ops) {
+      if (foundBlog) {
         blog.value.title = foundBlog.title
         blog.value.status = foundBlog.status
         blog.value.category = foundBlog.category
         blog.value.content = foundBlog.content
-      } else {
-        blog.value.content = { ops: [] }
       }
     })
-
-    watch(blog, () => {
-      if (blog){
-      console.log(blog.value.content)}
-    });
-
-    const modules = [
-      {
-        name: 'quillImageCompress',
-        module: ImageCompress,
-      },
-      {
-        name: 'magicUrl',
-        module: MagicUrl,
-      }
-    ]
 
     const updateCurrentComponent = () => {
       emit('updateCurrentComponent', 'dashboard')
     }
 
-    const updateContent = (newContent) => {
-      blog.value.content = { ops: newContent };
-    }
-
-    return { blog, modules, updateCurrentComponent, saveBlog, toast, updateContent }
+    return { blog, updateCurrentComponent, saveBlog, toast, editor: ClassicEditor }
   },
 }
 </script>
@@ -107,7 +83,7 @@ export default {
 
     <div class="row">
       <div class="col-12">
-        <QuillEditor theme="snow" toolbar="full" :modules="modules" :modelValue="blog.content" @update:modelValue="updateContent" />
+        <ckeditor :editor="editor" v-model="blog.content"></ckeditor>
       </div>
     </div>
   </div>
