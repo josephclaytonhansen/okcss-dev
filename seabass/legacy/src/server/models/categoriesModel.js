828c53365689479767c938ecc447f6a6d4c6dfd7
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-12-26 15:21:32 -0600

    remove legacy seabass

--- seabass/legacy/src/server/models/categoriesModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-
-const categorySchema = new mongoose.Schema({
-    name: {type:String, index: true},
-})
-
-const Category = mongoose.model('Category', categorySchema)
-Category.createIndexes()
-export default Category
