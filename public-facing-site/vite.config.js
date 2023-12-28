d5aed80583b4ac5f69453584bc9032e54a69142a
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2023-12-27 20:18:14 -0600

    Update vite.config.js

--- public-facing-site/vite.config.js
+++ public-facing-site/vite.config.js
@@ -4,6 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@vue/server-renderer': '/dev/null'
+    }
+  },
   plugins: [vue()],
   server: {
     port: 5280,
