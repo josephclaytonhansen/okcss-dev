f163d326be12bb55266f1f3a10ab54faa6a19e23
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-01-04 18:56:47 -0600

    comments

--- weasel-backend/routes/newsCommentRoutes.js
+++ weasel-backend/routes/newsCommentRoutes.js
@@ -12,8 +12,8 @@ import {
 
 router.route('/').post(getNewsComments)
 router.route('/:id').post(getNewsCommentById).delete(deleteNewsComment)
-router.route('/post/:post').post(getNewsCommentByPost)
+router.route('/post').post(getNewsCommentByPost)
 router.route('/create').post(createNewsComment)
-router.route('/toggle/:id').post(toggleVisibilityNewsComment)
+router.route('/toggle').post(toggleVisibilityNewsComment)
 
 export default router
