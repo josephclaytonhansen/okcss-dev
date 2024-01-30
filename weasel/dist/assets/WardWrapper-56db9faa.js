5e20ae847bb5152c4c3cfb2ea8f687ffdc420411
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-01-30 09:55:23 -0600

    delete contact

--- weasel/dist/assets/WardWrapper-56db9faa.js
+++ weasel/dist/assets/WardWrapper-56db9faa.js
@@ -1,4 +1,4 @@
-import{c as ge,d as f,e as p,f as b,t as A,n as z,j as _,p as ce,h as E,r as nt,q as V,s as lt,C as $i,T as Bi,x as Ve,w as Ro,y as B,F as C,k as W,z as $,A as ne,B as Z,D as xe,E as Gn,g as Q,G as Hi,H as Fi,i as Da,I as Ea,J as At,K as _t,a as Xe,L as Ui,l as Wi,M as ji,o as qi,b as It}from"./index-b1c8d10c.js";import{_ as Zi}from"./Image-fe300c71.js";/**
+import{c as ge,d as f,e as p,f as b,t as A,n as z,j as _,p as ce,h as E,r as nt,q as V,s as lt,C as $i,T as Bi,x as Ve,w as Ro,y as B,F as C,k as W,z as $,A as ne,B as Z,D as xe,E as Gn,g as Q,G as Hi,H as Fi,i as Da,I as Ea,J as At,K as _t,a as Xe,L as Ui,l as Wi,M as ji,o as qi,b as It}from"./index-4803b29d.js";import{_ as Zi}from"./Image-76e0fcad.js";/**
  * @license lucide-vue-next v0.294.0 - ISC
  *
  * This source code is licensed under the ISC license.
