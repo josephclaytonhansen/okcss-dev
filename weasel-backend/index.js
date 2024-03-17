dfcb8f1a2a2ad29d07f240f1fc05d8b2c4e12c6e
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-03-16 21:21:20 -0500

    Update index.js

--- weasel-backend/index.js
+++ weasel-backend/index.js
@@ -153,6 +153,7 @@ app.use('/api/hc-reports/create/new', (req, res, next) => {
         console.error(err)
         res.status(500).json({ message: err })
     })
+    next()
 })
 
 app.use('/api/hc-reports', (req, res, next) => {
