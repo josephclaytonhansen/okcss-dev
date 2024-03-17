6c19cec17a1fe2b90ee19fa70b94650a1ff939e8
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2024-03-16 20:42:26 -0500

    Update hc_reportRoutes.js

--- weasel-backend/routes/hc_reportRoutes.js
+++ weasel-backend/routes/hc_reportRoutes.js
@@ -1,4 +1,38 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
+import db from '../mongo.js'
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.zoho.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_FROM_USERNAME,
+      pass: process.env.EMAIL_FROM_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  })
+
+const sendHcMail = (email) => {
+    try{
+    transporter.sendMail({
+        from: 'internal@josephhansen.dev',
+      to: email, 
+      subject: 'A new high council report is available for you to review',
+      text: 'A new report has been added at https://highcouncil.okcsouthstake.org. \nDO NOT REPLY TO THIS EMAIL'
+    }, function(error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    })} catch (error) {
+        console.log(error)
+    }
+}
+
 const router = express.Router()
 import dotenv from 'dotenv'
 dotenv.config()
@@ -13,6 +47,7 @@ import {
     updateHighCouncilReport
 } from '../controllers/hc_reportController.js'
 
+
 router.route('/').post(getHighCouncilReports)
 router.route('/:id').get(getHighCouncilReportById).delete(deleteHighCouncilReport).put(updateHighCouncilReport)
 router.route('/counselor/:counselor').get(getHighCouncilReportsByCounselor)
@@ -25,6 +60,20 @@ router.route('/pin/check').post((req, res) => {
         res.status(401).send({ message: 'Invalid PIN' })
     }
 })
-router.route('/create/new').post(createHighCouncilReport)
+
+router.route('/create/new').post(
+    () => {
+        try {
+        db.getHighCouncilEmails().then((emails) => {
+            emails.forEach((email) => {
+                sendHcMail(email)
+            })
+        })
+        createHighCouncilReport} catch (error) {
+            console.log(error)
+        }
+    }
+
+)
 
 export default router
